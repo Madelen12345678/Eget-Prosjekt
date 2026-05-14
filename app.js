5b54f6ca681da3e62616b13dfc1beccb86cdb3a3
Madelen12345678 <madelenvn@icloud.com>
2026-05-14 15:47:34 +0200

    endringer

--- app.js
+++ app.js
@@ -11,3 +11,31 @@
                 info.classList.remove("active")
             })
         }
+
+
+const navEl = document.querySelector(".nav-links")
+const burgerEl = document.querySelector(".burger")
+const closeEl = document.querySelector(".close")
+const burgerContainerEl = document.querySelector(".burger-container")
+
+burgerContainerEl.addEventListener("click", function () {
+    navEl.classList.toggle("active")
+    burgerEl.classList.toggle("active")
+    closeEl.classList.toggle("active")
+}) 
+
+
+
+const btnEls = document.querySelectorAll(".story-link")
+const imgEls = document.querySelectorAll(".img")
+
+for (let btnEl of btnEls) {
+    btnEl.addEventListener("click", function(){
+
+        const imgEl = btnEl.parentElement.querySelector("img")
+        imgEl.classList.toggle("active")
+
+        const pEl = btnEl.parentElement.querySelector(".more")
+        pEl.classList.toggle("active")
+    })
+}
